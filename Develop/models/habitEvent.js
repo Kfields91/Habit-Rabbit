ca7d990a4ccce0a801a98507b7edddd1efb753d1
puakehaulani <puakehaulani@gmail.com>
2020-12-17 20:24:17 -0800

    freeze table added

--- Develop/models/habitEvent.js
+++ Develop/models/habitEvent.js
@@ -1,6 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
     var HabitEvent = sequelize.define("HabitEvent", {
         date: DataTypes.DATE
+    }, {
+        timestamps: false
+    }, {
+        freezeTableName: true
     });
     HabitEvent.associate = function (models) {
         HabitEvent.hasOne(models.Habit);
