7018d92b2acbc2f306f22164d965e8888f017ac0
sweetbabyjay <jayfly64@gmail.com>
2020-12-19 13:55:27 -0800

    added partials for handlebars to create a working navbar

--- Develop/config/config.js
+++ Develop/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     "development": {
         "username": "root",
-        "password": process.env.DBPASSWORD,
+        "password": "password",
         "database": "habitrabbitdb",
         "host": "127.0.0.1",
         "dialect": "mysql"
